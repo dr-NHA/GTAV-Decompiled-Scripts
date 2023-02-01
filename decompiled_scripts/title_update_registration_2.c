0fd81a4f76e096bb773d0d45fce67de96f804d6e
dr-NHA <56168811+dr-NHA@users.noreply.github.com>
2023-02-02 05:25:55 +1030

    title_update_registration_2.c Uploaded @ 1/02/2023 6:55:56 PM

---
+++ decompiled_scripts/title_update_registration_2.c
@@ -0,0 +1,42 @@
+void __EntryFunction__()
+{
+	Global_4456520 = -1;
+	Global_4456521 = -1;
+	Global_4456522 = -1;
+	Global_4456523 = -1;
+	Global_4456524 = -1;
+	Global_4536658 = 1;
+	Global_4536660 = 1;
+	Global_4536661 = 1;
+	Global_4536672 = -1;
+	Global_4538682 = 16003972;
+	Global_4539500 = -1;
+	Global_4539501 = -1;
+	Global_4539502 = -99;
+	Global_4539503 = -1;
+	Global_4539504 = -1;
+	Global_4539505 = -99;
+	Global_4539506 = -1;
+	Global_4539507 = -1;
+	Global_4539508 = -99;
+	Global_4539509 = -1;
+	Global_4539510 = -1;
+	Global_4539511 = -99;
+	Global_4539536 = -99;
+	StringCopy(&Global_4539856, "", 64);
+	StringCopy(&Global_4539873, "", 64);
+	Global_4539889 = 1;
+	Global_4540399 = -1;
+	Global_4540400 = -1;
+	Global_4540401 = 1;
+	Global_4540628 = -1;
+	Global_4540728 = 1;
+	Global_4540729 = 1;
+	Global_4542573 = 1;
+	StringCopy(&Global_4542586, "", 16);
+	Global_4542596 = 8f;
+	Global_4543292 = 12;
+	Global_4543293 = 12;
+	Global_4585356 = 100f;
+}
+
