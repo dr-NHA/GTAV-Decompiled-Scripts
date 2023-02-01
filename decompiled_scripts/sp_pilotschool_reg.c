eb33452444b0dd2bdbacbc3a6098166a3d3a4027
dr-NHA <56168811+dr-NHA@users.noreply.github.com>
2023-02-02 05:24:39 +1030

    sp_pilotschool_reg.c Uploaded @ 1/02/2023 6:54:40 PM

---
+++ decompiled_scripts/sp_pilotschool_reg.c
@@ -0,0 +1,13 @@
+void __EntryFunction__()
+{
+	unk_0xA2CDB686AB549D8A(&Global_3145728, 1, 1);
+	func_1();
+	unk_0xB6F769D890A34B99();
+	unk_0x0B0C9A0F9AAEB7F0(&Global_4543155, 1);
+}
+
+void func_1()
+{
+	unk_0xA40FAFA53DFF4362(&Global_3145728, "PILOT_SCHOOL_DUMMY_DATA");
+}
+
