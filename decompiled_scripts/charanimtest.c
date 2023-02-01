d6b7b9a7fd0c53814959434ca096e95d36412108
dr-NHA <56168811+dr-NHA@users.noreply.github.com>
2023-02-02 05:27:32 +1030

    charanimtest.c Uploaded @ 1/02/2023 6:57:33 PM

---
+++ decompiled_scripts/charanimtest.c
@@ -0,0 +1,21 @@
+void __EntryFunction__()
+{
+	if (unk_0x15CCE8886267624F())
+	{
+		unk_0x10B228D2FDB7AF16(500);
+	}
+	if (unk_0x96CFB880BAC634CE(18))
+	{
+		func_1();
+	}
+	while (true)
+	{
+		wait(0);
+	}
+}
+
+void func_1()
+{
+	unk_0xBBC29EBE6E1A48FA();
+}
+
