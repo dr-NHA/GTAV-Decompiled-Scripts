e73b431c6b4d08590797a6d7157e02427f911944
dr-NHA <56168811+dr-NHA@users.noreply.github.com>
2023-02-02 04:14:26 +1030

    density_test.c Uploaded @ 1/02/2023 5:44:00 PM

---
+++ decompiled_scripts/density_test.c
@@ -0,0 +1,55 @@
+#region Local Var
+	bool bLocal_0 = 0;
+	bool bLocal_1 = 0;
+	float fLocal_2 = 0f;
+	float fLocal_3 = 0f;
+	float fLocal_4 = 0f;
+	float fLocal_5 = 0f;
+	float fLocal_6 = 0f;
+	float fLocal_7 = 0f;
+	float fLocal_8 = 0f;
+#endregion
+
+void __EntryFunction__()
+{
+	bLocal_0 = true;
+	bLocal_1 = true;
+	fLocal_2 = 1f;
+	fLocal_3 = 1f;
+	fLocal_4 = 1f;
+	fLocal_5 = 1f;
+	fLocal_6 = 1f;
+	fLocal_7 = 1f;
+	fLocal_8 = 1f;
+	unk_0x925970A93719CADE(1);
+	if (unk_0x96CFB880BAC634CE(3))
+	{
+		func_1();
+	}
+	while (true)
+	{
+		wait(0);
+		if (!unk_0x4FAFF4BCB7633475(unk_0x4A8C381C258A124D()))
+		{
+			if (bLocal_0)
+			{
+				unk_0xA0265306DFF63938(fLocal_2);
+			}
+			unk_0x23D563236A543309(fLocal_3);
+			unk_0x40C1C94D5A5157C5(fLocal_4);
+			unk_0x5D50773C51925144(fLocal_5);
+			if (bLocal_1)
+			{
+				unk_0xF9A2335AB37CF17E(fLocal_6);
+			}
+			unk_0x0397A00D015A11D4(fLocal_7, fLocal_8);
+		}
+	}
+}
+
+void func_1()
+{
+	unk_0xAE03F542B985A69E(1);
+	unk_0xBBC29EBE6E1A48FA();
+}
+
