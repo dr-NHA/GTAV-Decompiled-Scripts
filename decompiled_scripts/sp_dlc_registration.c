e8dade20c179eb92a656813f07e5115d1a645a18
dr-NHA <56168811+dr-NHA@users.noreply.github.com>
2023-02-02 05:24:38 +1030

    sp_dlc_registration.c Uploaded @ 1/02/2023 6:54:39 PM

---
+++ decompiled_scripts/sp_dlc_registration.c
@@ -0,0 +1,16 @@
+void __EntryFunction__()
+{
+	int iVar0;
+	int iVar1;
+	
+	unk_0xA2CDB686AB549D8A(&Global_2883692, 1, 1);
+	unk_0xA40FAFA53DFF4362(&Global_2883692, "aFakeSavedInt");
+	unk_0xB6F769D890A34B99();
+	unk_0x0B0C9A0F9AAEB7F0(&Global_4543155, 0);
+	iVar0 = 1;
+	iVar1 = unk_0x7E1CF25229CF544D(1);
+	if (iVar0 != iVar1)
+	{
+	}
+}
+
